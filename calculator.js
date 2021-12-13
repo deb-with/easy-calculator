d1e29d94fc1735297054541bcff54d96ca3dcefc
DEBDULAL SAU <75360984+deb-with@users.noreply.github.com>
2021-12-14 00:30:15 +0530

    update

--- calculator.js
+++ calculator.js
@@ -1,5 +1,3 @@
-"use strict";
-
 var input = document.getElementById('input'), // input/output button
   number = document.querySelectorAll('.numbers div'), // number buttons
   operator = document.querySelectorAll('.operators div'), // operator buttons
